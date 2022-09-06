c3eafca0a7df18a973f4d940148d7fdd50629340
Ronnie Sheer <ronysheer@gmail.com>
2022-09-06 11:22:22 +0000

    feat: 02_01_end

---
+++ Ex_Files/02_01_end/main.py
@@ -0,0 +1,16 @@
+RUN_INDENTED = True
+
+message = "running unindented"
+
+if RUN_INDENTED:
+    message = "running indented"
+
+print(message)
+
+
+def my_function():
+    greet = "Hello"
+    return greet
+
+
+print(my_function())
