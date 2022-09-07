2850a9dcf30b5392c0bfeb16dd64a5f44753bd67
Ronnie Sheer <ronysheer@gmail.com>
2022-09-07 10:45:40 +0000

    add cs and local

--- Ex_Files/02_05_begin/main.py
+++ Ex_Files/02_05_begin/main.py
@@ -3,6 +3,8 @@ import os
 DEVELOPMENT = "development"
 PRODUCTION = "production"
 STAGING = "staging"
+CODE_SPACE = "code_space"
+LOCAL = "local"
 
 current_env = os.environ.get("ENV_NAME", DEVELOPMENT)
 
@@ -12,5 +14,7 @@ elif current_env == PRODUCTION:
     print("Production environment")
 elif current_env == STAGING:
     print("Staging environment")
+elif current_env in [CODE_SPACE, LOCAL]:
+    print("Codespace or local environment")
 else:
     print("Unknown environment")
