be6b28a6eb640d801ed05b86805ca579b9218726
Ronnie Sheer <ronysheer@gmail.com>
2022-09-07 13:01:21 +0000

    remove json import

--- Ex_Files/03_01_begin/main.py
+++ Ex_Files/03_01_begin/main.py
@@ -1,5 +1,4 @@
 import csv
-import json
 from pprint import pprint
 
 EINSTEIN_CSV = 'Albert,Einstein,1879-03-14,1955-04-18,Germany,"for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect",physics,1921'
