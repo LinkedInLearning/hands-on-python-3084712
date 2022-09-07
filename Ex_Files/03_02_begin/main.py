8a0dc58b83156de2c108a74835f70fdec0064152
Ronnie Sheer <ronysheer@gmail.com>
2022-09-07 11:24:00 +0000

    add age estimate

--- Ex_Files/03_02_begin/main.py
+++ Ex_Files/03_02_begin/main.py
@@ -1,5 +1,5 @@
 import csv
-import json
+from datetime import datetime
 from pprint import pprint
 
 EINSTEIN_CSV = 'Albert,Einstein,1879-03-14,1955-04-18,Germany,"for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect",physics,1921'
@@ -20,4 +20,8 @@ with open("laureates.csv", "r") as f:
 for laureate in laureates:
     if laureate["surname"] == "Einstein":
         pprint(laureate)
+        print("============")
+        year_date = datetime.strptime(laureate["year"], "%Y")
+        born_date = datetime.strptime(laureate["born"], "%Y-%m-%d")
+        print("age", year_date.year - born_date.year)
         break
