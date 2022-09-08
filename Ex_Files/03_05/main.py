05afee8137ac589241e6855c9aa5044295c2f470
Ronnie Sheer <ronysheer@gmail.com>
2022-09-08 12:24:27 +0000

    challange/solution

--- Ex_Files/03_05/main.py
+++ Ex_Files/03_05/main.py
@@ -29,6 +29,10 @@ with open("laureates.csv", "r") as f:
 laureates_beginning_with_a = []
 # LinkedIn learner code here
 
+for lareate in laureates:
+    if lareate['name'][0] == "A":
+        laureates_beginning_with_a.append(lareate)
+
 
 with open("laureates.json", "w") as f:
     json.dump(laureates_beginning_with_a, f, indent=2)
