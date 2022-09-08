0e1eac3b63536e67dd416e52c47faba0abf8f903
Ronnie Sheer <ronysheer@gmail.com>
2022-09-08 11:14:46 +0000

    clean up 03_03_begin

--- Ex_Files/03_03_begin/main.py
+++ Ex_Files/03_03_begin/main.py
@@ -2,8 +2,6 @@ import csv
 import json
 from pprint import pprint
 
-EINSTEIN_CSV = 'Albert,Einstein,1879-03-14,1955-04-18,Germany,"for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect",physics,1921'
-
 EINSTEIN = {
     "birthplace": "Germany",
     "name": "Albert",
