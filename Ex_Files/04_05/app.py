97c468679675864c1ff4f8b1dc7ec1e9a25d8ed4
Ronnie Sheer <ronysheer@gmail.com>
2022-09-11 09:44:05 +0000

    feat: 04_05 end state

---
+++ Ex_Files/04_05/app.py
@@ -0,0 +1,37 @@
+import csv
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+with open("laureates.csv", "r") as f:
+    reader = csv.DictReader(f)
+    laureates = list(reader)
+
+
+@app.route("/")
+def index():
+    # template found in templates/index.html
+    return render_template("index.html")
+
+
+@app.route("/laureates/")
+def laureate_list():
+    # template found in templates/laureate.html
+    results = []
+    if not request.args.get("flName"):
+        return jsonify(results)
+
+    search_string = request.args.get("flName").lower().strip()
+
+    # tip: remember that laureate["name"] contains a first name
+    for laureate in laureates:
+        full_name = f"{laureate['name'].lower()} {laureate['surname'].lower()}"
+        # your code here
+        if search_string in full_name:
+            results.append(laureate)
+
+    return jsonify(results)
+
+
+app.run(debug=True)
