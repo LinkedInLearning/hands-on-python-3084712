5d6dc74fd9301b7c1a65378d1e902cea80e68017
Ronnie Sheer <ronysheer@gmail.com>
2022-09-11 07:51:46 +0000

    04_02_end state

---
+++ Ex_Files/04_02_end/app.py
@@ -0,0 +1,31 @@
+import csv
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+with open("laureates.csv", "r") as f:
+    reader = csv.DictReader(f)
+    laureates = list(reader)
+
+
+@app.route("/")
+def index():
+    # template found in templates/index.html
+    return render_template("index.html")
+
+
+@app.route("/laureates/")
+def laureate():
+    # template found in templates/laureate.html
+    results = []
+    if not request.args.get("surname"):
+        return jsonify(results)
+
+    for laureate in laureates:
+        if request.args.get("surname").lower().strip() in laureate["surname"].lower():
+            results.append(laureate)
+    return jsonify(results)
+
+
+app.run(debug=True)
---
+++ Ex_Files/04_02_end/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+
+app.run(debug=True)
