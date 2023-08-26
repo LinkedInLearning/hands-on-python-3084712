79a7a9de86c6410e72a045e36c8b471679710aaa
Md Al Araf <77340161+mdalaraf2001@users.noreply.github.com>
2023-08-26 06:45:38 +0000

    string, formating

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -9,4 +9,7 @@ greet_format = "Hello {}"
 
 formatted = greet_format.format(name)
 
-print(intrupution, formatted)
+print(formatted)
+
+# print(formatted.lower())
+# print(formatted.replace("John", "chudir vai"))
