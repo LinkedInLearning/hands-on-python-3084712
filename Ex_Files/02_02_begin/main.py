2dcea7280958d11873df0e74e6b70fd05afcd745
C. L. Nolen <cl@followupboss.com>
2024-06-26 14:29:03 +0000

    Lesson commits

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -10,3 +10,7 @@ greet_format = "Hello {}"
 formatted = greet_format.format(name)
 
 print(intrupution, formatted)
+
+print(formatted.upper())
+print(formatted.lower())
+print(formatted.replace("John", "Paul"))
