3e03f2dda7833fa7a2b253579eff9b0f8ecc5be5
Ronnie Sheer <ronysheer@gmail.com>
2024-11-18 10:44:50 +0000

    revise_04_01

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -1,6 +1,8 @@
 import requests
 
 response = requests.get(
-    "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json")
+    "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json",
+)
 
 last_twenty_years = response.json()[1][:20]
+
