4fe793693d7372dbe8a836baf71c06e8a81d9b70
paolomagri <62219023+paolomagri@users.noreply.github.com>
2025-01-16 15:41:51 +0000

    loop for with example

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -5,6 +5,7 @@ i = 0
 while i < len(NAMES):
     print(NAMES[i], AGES[i])
     i += 1
+print("---------")
 
 for name in NAMES:
     print(name)
@@ -19,3 +20,5 @@ for i in range(5):
     print(i)
 
 # enumerate
+for i, name in enumerate(NAMES):
+    print(f"{i} {name}")
